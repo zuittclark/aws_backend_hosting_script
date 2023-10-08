3e51ca6a95dd4795338f7bfa1c2af97b82af3ef5
Jerry Clark <jerry.cabuntucan@tuitt.com>
2023-10-08 22:40:22 +0800

    updated script

--- setup.sh
+++ setup.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
-bc=1 # Replace this with your desired value for bc
+echo "What is your bootcamper number?"
+read bc # Replace this with your desired value for bc
 
+echo "Setting up for bootcamper$bc"
+echo "=================================="
 echo "Installing Node version 16.16.0..."
 curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 
 source ~/.nvm/nvm.sh
 nvm install 16.16.0
 nvm use 16.16.0
-echo "Node 16.16.0 setup success!"
 node -v
 echo "============================="
 
 echo "Installing project dependencies..."
-npm install
+cd .. && npm install
 echo "Dependencies installed successfully!"
 
+echo "===================================="
 echo "Adding service to crontab..."
-pm2 start index.js --name b${bc} --interpreter ~/.nvm/versions/node/v16.16.0/bin/node
+pm2 start index.js --name "b$bc" --interpreter ~/.nvm/versions/node/v16.16.0/bin/node
 
-(crontab -l 2>/dev/null; echo "@reboot sh -c 'cd /home/bootcamper${bc}/app && pm2 start index.js --name b${bc} --interpreter ~/.nvm/versions/node/v16.16.0/bin/node'") | crontab -
+(crontab -l 2>/dev/null; echo "@reboot sh -c 'cd /home/bootcamper$bc/app && pm2 start index.js --name b$bc --interpreter ~/.nvm/versions/node/v16.16.0/bin/node'") | crontab -
+
 
 echo "Added service to crontab!"
 crontab -l
-
+echo "===================================="
 echo "Setup script executed successfully!"
