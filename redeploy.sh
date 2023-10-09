a244115f5cdfd9edf2ab9ce85edefaf685654db7
Jerry Clark <jerry.cabuntucan@tuitt.com>
2023-10-09 19:49:05 +0800

    minor updates

--- redeploy.sh
+++ redeploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-
+echo "=========================================="
 echo "Pulling changes from remote repository..."
+echo "=========================================="
 git pull
-echo "====================================="
-
+echo "=========================================="
 echo "Restarting pm2 services..."
+echo "=========================================="
 pm2 restart all
-
-echo "====================================="
-echo "Changes successfully deployed!"
+echo "=========================================="
+echo "Changes successfully deployed!"      
+echo "=========================================="
