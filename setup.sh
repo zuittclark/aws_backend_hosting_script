a87d219a2df74a30f65dbd0eb23bf88f90276545
Jerry Clark <jerry.cabuntucan@tuitt.com>
2023-10-09 17:03:04 +0800

    minor fix

--- setup.sh
+++ setup.sh
@@ -8,18 +8,17 @@ fi
 
 bc="$1"
 
-
 echo "Setting up for bootcamper$bc"
-echo "=================================="
+
+echo "==================================="
 echo "Installing Node version 16.16.0..."
 curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-
 source ~/.nvm/nvm.sh
 nvm install 16.16.0
 nvm use 16.16.0
 node -v
-echo "============================="
 
+echo "===================================="
 echo "Installing project dependencies..."
 npm install
 echo "Dependencies installed successfully!"
@@ -28,10 +27,13 @@ echo "===================================="
 echo "Adding pm2 service..."
 pm2 start index.js --name "b$bc" --interpreter ~/.nvm/versions/node/v16.16.0/bin/node
 echo "Done adding pm2 service!"
+
+echo "===================================="
 echo "Adding service to crontab..."
 repo_name=$(basename "$PWD")
 (crontab -l 2>/dev/null; echo "@reboot sh -c 'cd /home/bootcamper$bc/$repo_name && pm2 start index.js --name b$bc --interpreter ~/.nvm/versions/node/v16.16.0/bin/node'") | crontab -
 echo "Added service to crontab!"
 crontab -l
+
 echo "===================================="
 echo "Setup script executed successfully!"
