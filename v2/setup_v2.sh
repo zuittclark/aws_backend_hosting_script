c37e604706e01ed6289abcf7b82c23f07d42b73b
Jerry Clark <jerry.cabuntucan@tuitt.com>
2023-11-13 14:27:30 +0800

    updated cron commands

--- v2/setup_v2.sh
+++ v2/setup_v2.sh
@@ -9,7 +9,7 @@ fi
 bc="$1"
 
 echo "Setting up production environment for bootcamper$bc"
-echo "\n==================================="
+echo "\n ==================================="
 echo "INSTALLING NODEJS VERSION 16.16.0..."
 curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 source ~/.nvm/nvm.sh
@@ -18,18 +18,18 @@ nvm use 16.16.0
 node -v
 
 
-echo "\n===================================="
+echo "\n ===================================="
 echo "INSTALLING EXPRESSJS PROJECT DEPENDENCIES..."
 npm install || { echo "Error installing npm dependencies"; exit 1; }
 echo "~> Dependencies installed successfully!"
 
-echo "\n===================================="
+echo "\n ===================================="
 echo "INSTALLING WEBHOOK FOR DEPLOYMENTS..."
 git clone --depth=1 https://github.com/zuittclark/aws_backend_hosting_script.git --branch=master --single-branch v2-temp || { echo "Error cloning"; exit 1; }
 mv v2-temp/v2/webhook webhook && rm -rf v2-temp 
 echo "~> Web Hook installed successfully!"
 
-echo "\n===================================="
+echo "\n ===================================="
 echo "INITIALIZING PM2 SERVICE..."
 echo "starting project server..."
 pm2 start index.js --name "b$bc" --interpreter ~/.nvm/versions/node/v16.16.0/bin/node || { echo "Error starting PM2 service for project server"; exit 1; }
@@ -39,18 +39,18 @@ pm2 start webhook/webHookServer.js --name "webhook-server$bc" --interpreter ~/.n
 echo "starting web hook server DONE!"
 echo "~> Done adding pm2 service!"
 
-echo "\n===================================="
+echo "\n ===================================="
 echo "ADDING SERVICE TO CRONTAB..."
 current_dir="$PWD"
 #This updated command removes duplication of the cron command on multiple execution of the script
 cron_command1="@reboot sh -c 'cd $current_dir && pm2 start index.js --name b$bc --interpreter ~/.nvm/versions/node/v16.16.0/bin/node'"
 cron_command2="@reboot sh -c 'cd $current_dir && pm2 start webhook/webHookServer.js --name webhook-server --interpreter ~/.nvm/versions/node/v16.16.0/bin/node -- $bc'"
-
-{ echo "$cron_command1\n$cron_command2"; } | crontab - || { echo "Error overwriting crontab"; exit 1; }
+combined_cron_commands="$cron_command1"$'\n'"$cron_command2"
+{ echo "$combined_cron_commands"; } | crontab - || { echo "Error overwriting crontab"; exit 1; }
 
 echo "~> Updated crontab successfully!"
 crontab -l
-echo "\n~> Setup script executed successfully!"
-echo "\n==================================================================="
+echo "\n ~> Setup script executed successfully!"
+echo "\n ==================================================================="
 echo "Congratulations! You have successfully deployed your Backend API."
 echo "==================================================================="
