dc3be0d9030ab5ccc76fb62c85ed1f1f25b0a492
Jerry Clark <jerry.cabuntucan@tuitt.com>
2023-11-13 14:39:38 +0800

    bug fixes

--- v2/setup_v2.sh
+++ v2/setup_v2.sh
@@ -8,9 +8,9 @@ fi
 
 bc="$1"
 
-echo "Setting up production environment for bootcamper$bc"
-echo "\n ==================================="
-echo "INSTALLING NODEJS VERSION 16.16.0..."
+echo -e "Setting up production environment for bootcamper$bc"
+echo -e "\n==================================="
+echo -e "INSTALLING NODEJS VERSION 16.16.0..."
 curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 source ~/.nvm/nvm.sh
 nvm install 16.16.0 
@@ -18,29 +18,29 @@ nvm use 16.16.0
 node -v
 
 
-echo "\n ===================================="
+echo -e "\n===================================="
 echo "INSTALLING EXPRESSJS PROJECT DEPENDENCIES..."
 npm install || { echo "Error installing npm dependencies"; exit 1; }
 echo "~> Dependencies installed successfully!"
 
-echo "\n ===================================="
-echo "INSTALLING WEBHOOK FOR DEPLOYMENTS..."
+echo -e "\n===================================="
+echo -e "INSTALLING WEBHOOK FOR DEPLOYMENTS..."
 git clone --depth=1 https://github.com/zuittclark/aws_backend_hosting_script.git --branch=master --single-branch v2-temp || { echo "Error cloning"; exit 1; }
 mv v2-temp/v2/webhook webhook && rm -rf v2-temp 
-echo "~> Web Hook installed successfully!"
+echo -e "~> Web Hook installed successfully!"
 
-echo "\n ===================================="
+echo -e "\n===================================="
 echo "INITIALIZING PM2 SERVICE..."
 echo "starting project server..."
 pm2 start index.js --name "b$bc" --interpreter ~/.nvm/versions/node/v16.16.0/bin/node || { echo "Error starting PM2 service for project server"; exit 1; }
-echo "starting project server DONE!"
-echo "\nstarting web hook server..."
+echo -e "starting project server DONE!"
+echo -e "\nstarting web hook server..."
 pm2 start webhook/webHookServer.js --name "webhook-server$bc" --interpreter ~/.nvm/versions/node/v16.16.0/bin/node -- $bc || { echo "Error starting PM2 service for webhook server"; exit 1; }
-echo "starting web hook server DONE!"
-echo "~> Done adding pm2 service!"
+echo -e "starting web hook server DONE!"
+echo -e "~> Done adding pm2 service!"
 
-echo "\n ===================================="
-echo "ADDING SERVICE TO CRONTAB..."
+echo -e "\n===================================="
+echo -e "ADDING SERVICE TO CRONTAB..."
 current_dir="$PWD"
 #This updated command removes duplication of the cron command on multiple execution of the script
 cron_command1="@reboot sh -c 'cd $current_dir && pm2 start index.js --name b$bc --interpreter ~/.nvm/versions/node/v16.16.0/bin/node'"
@@ -50,7 +50,7 @@ combined_cron_commands="$cron_command1"$'\n'"$cron_command2"
 
 echo "~> Updated crontab successfully!"
 crontab -l
-echo "\n ~> Setup script executed successfully!"
-echo "\n ==================================================================="
-echo "Congratulations! You have successfully deployed your Backend API."
-echo "==================================================================="
+echo -e "\n~> Setup script executed successfully!"
+echo -e "\n==================================================================="
+echo -e "Congratulations! You have successfully deployed your Backend API."
+echo -e "==================================================================="
