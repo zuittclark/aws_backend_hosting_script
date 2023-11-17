ac8c69dffaf29f1842eb238524929a6ab32ea3d5
Jerry Clark <jerry.cabuntucan@tuitt.com>
2023-11-17 12:44:08 +0800

    minor fixes

--- v2.1/inst/webhook/webHookServer.js
+++ v2.1/inst/webhook/webHookServer.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 const app = express();
 const PORT = 4100;
-const sudoPassword = "1234"; // quite questionable choice HAHAHA but it works for now. This should be stored in an enviroment variable.
+const sudoPassword = "1234"; // quite questionable choice HAHAHA but it works for now. This should be a more secure password and is stored in an enviroment variable.
 // Parse JSON requests
 app.use(express.json());
 
