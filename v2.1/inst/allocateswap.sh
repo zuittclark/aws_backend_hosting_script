36aa504c7c2632f5d8543178b3311c3c10864606
Jerry Clark <jerry.cabuntucan@tuitt.com>
2023-11-27 16:24:19 +0800

    updated allocateswap script

--- v2.1/inst/allocateswap.sh
+++ v2.1/inst/allocateswap.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-
 echo -e "===================================="
-echo -e "Creating swap file ..."
+echo -e "Setting up swap memory ..."
 echo -e "===================================="
-
-sudo fallocate -l 2G /swapfile
-sudo chmod 600 /swapfile
-sudo mkswap /swapfile
-sudo swapon /swapfile
-echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+SWAPFILE="/swapfile"
+if [ -e "$SWAPFILE" ]; then
+    echo -e "Swap file already exists. Skipping creation."
+else
+    # Create 1 gigabytes swap file
+    sudo fallocate -l 1G "$SWAPFILE"
+    sudo chmod 600 "$SWAPFILE"
+    sudo mkswap "$SWAPFILE"
+    sudo swapon "$SWAPFILE"
+    # Add swap file to /etc/fstab
+    echo "$SWAPFILE none swap sw 0 0" | sudo tee -a /etc/fstab
+    echo -e "Swap file created and activated."
+fi
 sudo swapon --show
-
 echo -e "===================================="
-echo -e "Swap file created successfully."
+echo -e "Swap memory setup successful!"
 echo -e "You can check the swap memory in the task manager using htop."
 echo -e "===================================="
